0e720b75eab4ae9f9aa0868d28889acc9645a16f
Dragana <peric.dr@gmail.com>
2020-07-06 20:23:54 +0200

    Initial commit. Creates class Car

---
+++ src/main/java/main/factory/Car.java
@@ -0,0 +1,13 @@
+
+package main.factory;
+
+public class Car {
+    
+    String model;
+    int buildYear;
+    String color;
+    int maxSpeed;
+    int ccm;
+    int consumption;
+    
+}
---
+++ src/main/java/main/factory/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.factory;
+
+/**
+ *
+ * @author ivand
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Pocetak");
+        
+        int x = 5;
+        Car auto1 = new Car();
+        
+        auto1.model = "Audi";
+        auto1.buildYear = 2003;
+        auto1.ccm = 2000;
+        
+        System.out.println("Model automobila 1 je: " +auto1.model);
+        
+        Car auto2 = new Car();
+        auto2.model = "Fiat";
+        auto2.buildYear = 2019;
+        
+        System.out.println("Model automobila 2 je: " +auto2.model);
+        
+
+    }
+    
+}
