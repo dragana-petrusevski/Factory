8b902314e1abe7330a7942960cc182919e0574dc
Dragana <peric.dr@gmail.com>
2020-07-13 20:29:01 +0200

    refactors class Car, adds getters and setters

--- src/main/java/main/factory/Main.java
+++ src/main/java/main/factory/Main.java
@@ -8,26 +8,33 @@ package main.factory;
 public class Main {
 
     /**
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Pocetak");
         
-        
+        int x = 5;
         Car auto1 = new Car();
         
-        auto1.model = "Audi";
-        auto1.buildYear = 2003;
-        auto1.ccm = 2000;
+        auto1.setModel("Audi");
+        auto1.setBuildYear(2003);
+        auto1.setCcm(2000);
         
-        System.out.println("Model automobila 1 je: " +auto1.model);
+        System.out.println("Model automobila 1 je: " +auto1.getModel());
+        auto1.printAttributes();
         
+        int y = 29;
         Car auto2 = new Car();
-        auto2.model = "Fiat";
-        auto2.buildYear = 2019;
+        auto2.setModel("Fiat");
+        auto2.setBuildYear(2019);
+        auto2.printAttributes();
         
-        System.out.println("Model automobila 2 je: " +auto2.model);
+        System.out.println("Model automobila 2 je: " +auto2.getModel());
         
+        
+        Car lada = new Car();
+        lada.setModel("Lada");
 
     }
 
